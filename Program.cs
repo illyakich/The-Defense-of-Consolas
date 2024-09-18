8d464c36cf27e849a62c037dbaa4796e6f4f1139
ПК <@DESKTOP-5UNSHBK>
2024-09-18 21:42:44 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Title = "Defense of Consolas";
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Target row? ");
+int row  = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Target column? ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Deploy to: ");
+Console.WriteLine($"({row}, {column - 1})");
+Console.WriteLine($"({row - 1}, {column})");
+Console.WriteLine($"({row}, {column + 1})");
+Console.WriteLine($"({row + 1}, {column})");
+// 200xp earned yay!!
